f307b6c32809da997b2f28bc21d5d9102092778b
Kevin Herro <kvherro@gmail.com>
2023-06-13 16:23:04 -0700

    deck

--- nuts/src/lib.rs
+++ nuts/src/lib.rs
@@ -1,34 +1,28 @@
+use itertools::Itertools;
+
+const RANKS: [&'static str; 13] = [
+    "A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2",
+];
+
+const SUITS: [&'static str; 4] = ["♠", "♣", "♦", "♥"];
+
 pub struct Card {
     rank: &'static str,
     suit: &'static str,
 }
 
-pub struct Cards {
-    deck: Vec<Card>,
+pub struct Deck {
+    cards: Vec<Card>,
 }
 
-impl Cards {
-    fn new() -> Cards {
-        // TODO: Add the rest of the deck.
-        let deck: Vec<Card> = vec![
-            Card {
-                rank: "A",
-                suit: "♠",
-            },
-            Card {
-                rank: "A",
-                suit: "♣",
-            },
-            Card {
-                rank: "A",
-                suit: "♦",
-            },
-            Card {
-                rank: "A",
-                suit: "♥",
-            },
-        ];
+impl Deck {
+    fn new() -> Deck {
+        let cards: Vec<Card> = RANKS
+            .iter()
+            .cartesian_product(&SUITS)
+            .map(|(&rank, &suit)| Card { rank, suit })
+            .collect();
 
-        Cards { deck }
+        Deck { cards }
     }
 }
