4582434265fa4dfde6ae620599c71d7bb927c102
Kevin Herro <kvherro@gmail.com>
2023-06-15 20:06:08 -0700

    relax

--- nuts/src/lib.rs
+++ nuts/src/lib.rs
@@ -1,14 +1,39 @@
-use itertools::Itertools;
+use itertools::{Itertools, Tuples};
+use lazy_static::lazy_static;
+use rand::seq::SliceRandom;
+use std::collections::HashMap;
+use std::string::ToString;
+use utils::{Rank, Suit};
 
-const RANKS: [&'static str; 13] = [
-    "A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2",
+const RANKS: [Rank; 13] = [
+    Rank::Ace,
+    Rank::King,
+    Rank::Queen,
+    Rank::Jack,
+    Rank::Ten,
+    Rank::Nine,
+    Rank::Eight,
+    Rank::Seven,
+    Rank::Six,
+    Rank::Five,
+    Rank::Four,
+    Rank::Three,
+    Rank::Two,
 ];
 
-const SUITS: [&'static str; 4] = ["♠", "♣", "♦", "♥"];
+lazy_static! {
+    static ref SUITS: [Suit; 4] = [
+        Suit::Clubs(String::from("♣")),
+        Suit::Diamonds("♦".to_string()),
+        Suit::Hearts("♥".to_string()),
+        Suit::Spades("♠".to_string()),
+    ];
+}
 
+#[derive(Eq, PartialEq, Clone)]
 pub struct Card {
-    rank: &'static str,
-    suit: &'static str,
+    rank: Rank,
+    suit: Suit,
 }
 
 pub struct Deck {
@@ -16,13 +41,58 @@ pub struct Deck {
 }
 
 impl Deck {
-    fn new() -> Deck {
+    pub fn new() -> Deck {
         let cards: Vec<Card> = RANKS
             .iter()
-            .cartesian_product(&SUITS)
-            .map(|(&rank, &suit)| Card { rank, suit })
+            .clone()
+            .cartesian_product(SUITS.iter())
+            .map(|(rank, suit)| Card {
+                rank: rank.clone(),
+                suit: suit.clone(),
+            })
             .collect();
 
         Deck { cards }
+    }
+
+    fn shuffle(self) -> Vec<Card> {
+        let mut rng = rand::thread_rng();
+        let mut shuffled_deck = self.cards.clone();
+        shuffled_deck.shuffle(&mut rng);
+        shuffled_deck[..5].to_vec()
+    }
+}
+
+enum HandRank {
+    HighCard(u32),
+    OnePair(u32),
+    TwoPair(u32),
+    ThreeOfAKind(u32),
+    Straight(u32),
+    Flush(u32),
+    FullHouse(u32),
+    FourOfAKind(u32),
+    StraightFlush(u32),
+    RoyalFlush,
+}
+
+struct Nuts {
+    hand: [Card; 5],
+    nuts: (Card, Card),
+}
+
+impl Nuts {
+    fn new(&mut self, hand: [Card; 5]) {
+        self.hand = hand;
+        self.nuts = self.evaluate_hand();
+    }
+
+    fn evaluate_hand(&self) -> (Card, Card) {
+        (self.hand[0].clone(), self.hand[1].clone())
+    }
+
+    fn is_nuts(&self, tuple: (Card, Card)) -> bool {
+        (tuple.0 == self.nuts.0 || tuple.0 == self.nuts.1)
+            && (tuple.1 == self.nuts.0 || tuple.1 == self.nuts.1)
     }
 }
--- preflop/src/lib.rs
+++ preflop/src/lib.rs
@@ -1,66 +1,6 @@
 use std::fmt::{Debug, Display, Formatter};
 use std::str::FromStr;
 
-#[derive(Debug, Clone, Eq, PartialEq)]
-enum Rank {
-    Ace,
-    Two,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-    Ten,
-    Jack,
-    Queen,
-    King,
-}
-
-impl FromStr for Rank {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "A" => Ok(Rank::Ace),
-            "2" => Ok(Rank::Two),
-            "3" => Ok(Rank::Three),
-            "4" => Ok(Rank::Four),
-            "5" => Ok(Rank::Five),
-            "6" => Ok(Rank::Six),
-            "7" => Ok(Rank::Seven),
-            "8" => Ok(Rank::Eight),
-            "9" => Ok(Rank::Nine),
-            "T" => Ok(Rank::Ten),
-            "J" => Ok(Rank::Jack),
-            "Q" => Ok(Rank::Queen),
-            "K" => Ok(Rank::King),
-            _ => Err(format!("Invalid rank: {}", s)),
-        }
-    }
-}
-
-impl Display for Rank {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Rank::Ace => write!(f, "A"),
-            Rank::Two => write!(f, "2"),
-            Rank::Three => write!(f, "3"),
-            Rank::Four => write!(f, "4"),
-            Rank::Five => write!(f, "5"),
-            Rank::Six => write!(f, "6"),
-            Rank::Seven => write!(f, "7"),
-            Rank::Eight => write!(f, "8"),
-            Rank::Nine => write!(f, "9"),
-            Rank::Ten => write!(f, "T"),
-            Rank::Jack => write!(f, "J"),
-            Rank::Queen => write!(f, "Q"),
-            Rank::King => write!(f, "K"),
-        }
-    }
-}
-
 #[derive(Debug, Eq, PartialEq)]
 enum TablePosition {
     Early,
@@ -174,8 +114,8 @@ impl Display for SuitCombination {
 #[derive(Debug)]
 pub struct Context {
     table_position: TablePosition,
-    card_1: Rank,
-    card_2: Rank,
+    card_1: utils::Rank,
+    card_2: utils::Rank,
     suit_combination: SuitCombination,
     action: Action,
     stack_size: StackSize,
@@ -213,12 +153,12 @@ impl FromStr for Context {
             Err(e) => return Err(e),
         };
 
-        let card_1: Rank = match parts[1].trim().parse() {
+        let card_1: utils::Rank = match parts[1].trim().parse() {
             Ok(rank) => rank,
             Err(e) => return Err(e),
         };
 
-        let card_2: Rank = match parts[2].trim().parse() {
+        let card_2: utils::Rank = match parts[2].trim().parse() {
             Ok(rank) => rank,
             Err(e) => return Err(e),
         };
